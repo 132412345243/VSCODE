e89d6e16e94fe1559a4482dafe55d6fc94580472
Ariel Lev <ariel.lev767@gmail.com>
2024-03-12 14:14:22 +0200

    type error

--- server/multiconn_server.py
+++ server/multiconn_server.py
@@ -6,7 +6,7 @@ import types
 sel = selectors.DefaultSelector()
 
 def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
+    conn, addr = sock.accept() 
     print(f"Accepted connection from {addr}")
     conn.setblocking(False)
     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
@@ -17,7 +17,7 @@ def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
+        recv_data = sock.recv(1024)  
         if recv_data:
             data.outb += recv_data
         else:
@@ -27,11 +27,11 @@ def service_connection(key, mask):
     if mask & selectors.EVENT_WRITE:
         if data.outb:
             print(f"Echoing {data.outb!r} to {data.addr}")
-            sent = sock.send(data.outb)  # Should be ready to write
+            sent = sock.send(data.outb)  
             data.outb = data.outb[sent:]
 
 
-host, port = sys.argv[1], int(sys.argv[2])
+host, port = sys.argv[0], int(sys.argv[0])
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
 lsock.listen()
