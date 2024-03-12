f35a2a41271f9fb32dad666f9eeefab580a1fdfe
Ariel Lev <ariel.lev767@gmail.com>
2024-03-12 14:39:32 +0200

    new way

--- server/multiconn_server.py
+++ server/multiconn_server.py
@@ -1,53 +1,42 @@
-import sys
-import socket
-import selectors
-import types
+import socket 
 
-sel = selectors.DefaultSelector()
+if __name__ == '__main__': 
+	host = '127.0.0.1'
+	port = 8080
+	totalclient = int(input('Enter number of clients: ')) 
 
-def accept_wrapper(sock):
-    conn, addr = sock.accept() 
-    print(f"Accepted connection from {addr}")
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
-     
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print(f"Closing connection to {data.addr}")
-            sel.unregister(sock)
-            sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print(f"Echoing {data.outb!r} to {data.addr}")
-            sent = sock.send(data.outb)  
-            data.outb = data.outb[sent:]
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	sock.bind((host, port)) 
+	sock.listen(totalclient) 
+	connections = [] 
+	print('Initiating clients') 
+	for i in range(totalclient): 
+		conn = sock.accept() 
+		connections.append(conn) 
+		print('Connected with client', i+1) 
 
+	fileno = 0
+	idx = 0
+	for conn in connections: 
+		idx += 1
+		data = conn[0].recv(1024).decode() 
 
-host, port = sys.argv[0], int(sys.argv[0])
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((host, port))
-lsock.listen()
-print(f"Listening on {(host, port)}")
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
+		if not data: 
+			continue 
+		filename = 'output'+str(fileno)+'.txt'
+		fileno = fileno+1
+		fo = open(filename, "w") 
+		while data: 
+			if not data: 
+				break
+			else: 
+				fo.write(data) 
+				data = conn[0].recv(1024).decode() 
 
-try:
-    while True:
-        events = sel.select(timeout=None)
-        for key, mask in events:
-            if key.data is None:
-                accept_wrapper(key.fileobj)
-            else:
-                service_connection(key, mask)
-except KeyboardInterrupt:
-    print("Caught keyboard interrupt, exiting")
-finally:
-    sel.close()
+		print() 
+		print('Receiving file from client', idx) 
+		print() 
+		print('Received successfully! New filename is:', filename) 
+		fo.close() 
+	for conn in connections: 
+		conn[0].close() 
